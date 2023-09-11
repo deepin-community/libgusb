5b33aa9b6a9bd202459e3043e0ea952ed71fb728
xzl <xiangzelong@deepin.org>
2023-09-11 11:11:47 +0800

    chore: update to 0.45
    Log:

--- gusb/gusb-endpoint.h
+++ gusb/gusb-endpoint.h
@@ -7,23 +7,30 @@
 
 #pragma once
 
-#include <glib-object.h>
-#include <gio/gio.h>
 #include <gusb/gusb-device.h>
 
 G_BEGIN_DECLS
 
-#define G_USB_TYPE_ENDPOINT (g_usb_endpoint_get_type ())
-G_DECLARE_FINAL_TYPE (GUsbEndpoint, g_usb_endpoint, G_USB, ENDPOINT, GObject)
+#define G_USB_TYPE_ENDPOINT (g_usb_endpoint_get_type())
+G_DECLARE_FINAL_TYPE(GUsbEndpoint, g_usb_endpoint, G_USB, ENDPOINT, GObject)
 
-guint8			g_usb_endpoint_get_kind			(GUsbEndpoint *endpoint);
-guint16			g_usb_endpoint_get_maximum_packet_size 	(GUsbEndpoint *endpoint);
-guint8 			g_usb_endpoint_get_polling_interval	(GUsbEndpoint *endpoint);
-guint8 			g_usb_endpoint_get_refresh		(GUsbEndpoint *endpoint);
-guint8 			g_usb_endpoint_get_synch_address	(GUsbEndpoint *endpoint);
-guint8		 	g_usb_endpoint_get_address		(GUsbEndpoint *endpoint);
-guint8		 	g_usb_endpoint_get_number		(GUsbEndpoint *endpoint);
-GUsbDeviceDirection	g_usb_endpoint_get_direction		(GUsbEndpoint *endpoint);
-GBytes *		g_usb_endpoint_get_extra		(GUsbEndpoint *endpoint);
+guint8
+g_usb_endpoint_get_kind(GUsbEndpoint *self);
+guint16
+g_usb_endpoint_get_maximum_packet_size(GUsbEndpoint *self);
+guint8
+g_usb_endpoint_get_polling_interval(GUsbEndpoint *self);
+guint8
+g_usb_endpoint_get_refresh(GUsbEndpoint *self);
+guint8
+g_usb_endpoint_get_synch_address(GUsbEndpoint *self);
+guint8
+g_usb_endpoint_get_address(GUsbEndpoint *self);
+guint8
+g_usb_endpoint_get_number(GUsbEndpoint *self);
+GUsbDeviceDirection
+g_usb_endpoint_get_direction(GUsbEndpoint *self);
+GBytes *
+g_usb_endpoint_get_extra(GUsbEndpoint *self);
 
 G_END_DECLS
--- gusb/gusb.h
+++ gusb/gusb.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
  *
- * Copyright (C) 2010-2011 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2010 Richard Hughes <richard@hughsie.com>
  *
  * SPDX-License-Identifier: LGPL-2.1+
  */
@@ -9,14 +9,15 @@
 
 #define __GUSB_INSIDE__
 
-#include <gusb/gusb-autocleanups.h>
+#include <gusb/gusb-bos-descriptor.h>
 #include <gusb/gusb-context.h>
-#include <gusb/gusb-interface.h>
-#include <gusb/gusb-endpoint.h>
-#include <gusb/gusb-source.h>
-#include <gusb/gusb-device.h>
+#include <gusb/gusb-device-event.h>
 #include <gusb/gusb-device-list.h>
-#include <gusb/gusb-version.h>
+#include <gusb/gusb-device.h>
+#include <gusb/gusb-endpoint.h>
+#include <gusb/gusb-interface.h>
+#include <gusb/gusb-source.h>
 #include <gusb/gusb-util.h>
+#include <gusb/gusb-version.h>
 
 #undef __GUSB_INSIDE__
